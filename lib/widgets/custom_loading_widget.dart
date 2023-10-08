8d65dfec944f7d1fc72c288b540ffe9c588afbf2
Vijay Bheda <bhedavijay489@gmail.com>
2023-10-08 15:40:43 +0530

    - iOS account and minor app configuration from Xcode - loading progress UI and package change

--- lib/widgets/custom_loading_widget.dart
+++ lib/widgets/custom_loading_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 import 'package:medic/_dart/_init.dart';
 
 class CustomLoadingWidget extends StatelessWidget {
@@ -6,10 +6,26 @@ class CustomLoadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Align(
+      alignment: FractionalOffset.center,
+      child: SizedBox(
+        height: 50,
+        width: 50,
+        child: LoadingIndicator(
+          colors: [AppColors.primaryColor],
+          indicatorType: Indicator.ballScale,
+          strokeWidth: 1,
+        ),
+      ),
+    );
+  }
+
+  /*@override
+  Widget build(BuildContext context) {
     return CupertinoActivityIndicator(
       color: AppColors.primaryColor,
       animating: true,
       radius: 20,
     );
-  }
+  }*/
 }
